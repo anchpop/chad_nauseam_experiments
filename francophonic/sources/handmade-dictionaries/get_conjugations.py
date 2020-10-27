785d9f16c106de18659fdb4b848dcb46b2b639d2
Andre Popovitch <andre@popovit.ch>
2020-10-27 01:50:02 -0400

    now I still need to rework how the word dictionary works

--- francophonic/sources/handmade-dictionaries/get_conjugations.py
+++ francophonic/sources/handmade-dictionaries/get_conjugations.py
@@ -1,4 +1,5 @@
 import pprint
+import yaml
 
 import requests
 from bs4 import BeautifulSoup
@@ -15,13 +16,17 @@ def grab_conjugation_soup(verb):
 
 
 def main():
+    verb = input("Verb? ")
+
     contents = {}
 
-    current_mode = ""x
+    current_mode = ""
 
-    with open("C:/Users/hyper/Desktop/Untitled-1.html", "r", encoding="utf-8") as f:
+    """with open("C:/Users/hyper/Desktop/Untitled-1.html", "r", encoding="utf-8") as f:
         text = f.read()
-        soup = BeautifulSoup(text, 'html.parser')
+        soup = BeautifulSoup(text, 'html.parser')"""
+    
+    soup = grab_conjugation_soup(verb)
 
     result_block = soup.findAll("div", {"class": "result-block-api"})[0]
     word_wrap_rows = result_block.findAll("div", {"class": "word-wrap-row"})
@@ -38,22 +43,26 @@ def main():
                     # print(current_mode)
                 if len(conjugations) == 1:
                     info = {}
-                    type_of_conjugation = conjugations[0].p.contents[0].strip()
+                    type_of_conjugation = conjugations[0].p.contents[0].strip().lower()
                     cons = conjugations[0].ul.findAll("li")
                     for con in cons:
                         i = [i.contents[0].strip().lower() for i in con.findAll("i")]
-                        info[tuple(i[:-1])] = i[-1]
+                        if len(i) == 1 and current_mode == 'infinitif':
+                            infinitive = i[0]
+                        elif len(i) >= 2:
+                            info[" - ".join(i[:-1])] = i[-1]
                     
                     # print("    " + type_of_conjugation)
-                    contents[current_mode] = contents.get(current_mode, []) + [{type_of_conjugation: info}]
+                    if len(info) > 0:
+                        contents[current_mode] = contents.get(current_mode, {}) | {type_of_conjugation: info}
 
     model = soup.find("span", {"tooltip": "See more info on the conjugation model and verbs which conjugate the same way."}).a.contents[0].strip()
     auxiliary = soup.find("span", {"tooltip": "The auxiliary verb used in the conjugation of the compounds forms."}).a.contents[0].strip()
-    print("model:", model)
-    print("auxiliary:", auxiliary)
     
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(contents)
+
+    output = {infinitive: [{'display': infinitive, 'pos': 'verb', 'conjugations': contents, 'translations': [''], 'model': model, 'auxiliary': auxiliary}]}
+    print(yaml.dump(output, allow_unicode=True))
 
 
 
