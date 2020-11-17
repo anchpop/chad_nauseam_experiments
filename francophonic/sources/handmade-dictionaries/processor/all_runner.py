0c632f3577847cfab4dcab045863eecc9c175970
Andre Popovitch <andre@popovit.ch>
2020-11-17 01:32:15 -0500

    small refactor in preperation of sentence associating

--- francophonic/sources/handmade-dictionaries/processor/all_runner.py
+++ francophonic/sources/handmade-dictionaries/processor/all_runner.py
@@ -1,6 +1,6 @@
 import processor.sources_analysis
-import processor.handmade_dictionary_combiner 
-import processor.auto_sentence_translator 
+import processor.handmade_dictionary_combiner
+import processor.auto_sentence_translator
 import processor.tts_generator
 import processor.handmade_dictionary_template_generator
 import processor.data_mover
@@ -10,28 +10,29 @@ from os.path import isfile, join
 import os
 
 
-
-
 def main():
-  analysis = processor.sources_analysis.do_analysis()
-    
-  
-  if processor.handmade_dictionary_combiner.main(analysis) != True:
-    return
+    analysis = processor.sources_analysis.do_analysis()
 
-  processor.print_stats.print_stats(analysis)
+    nodelete = False
+    if processor.handmade_dictionary_combiner.main(analysis) != True:
+        nodelete = True
+        if input("Continue without adding words? ").lower().strip()[0] != "y":
+            return
 
-  processor.auto_sentence_translator.main(analysis)
-  processor.tts_generator.main()
-  if isfile("handmade_dictionary.yaml"):
-    i = input("would you like to delete `handmade_dictionary.yaml` and generate a new template? (yes/no) ")
-    if i.strip() == "yes":
-      os.remove("handmade_dictionary.yaml")
-      processor.handmade_dictionary_template_generator.main(analysis)
-  else:
-    processor.handmade_dictionary_template_generator.main(analysis)
+    processor.print_stats.print_stats(analysis)
 
-  processor.data_mover.main(analysis)
+    processor.auto_sentence_translator.main(analysis)
+    processor.tts_generator.main()
+    if isfile("handmade_dictionary.yaml") and not nodelete:
+        i = input("would you like to delete `handmade_dictionary.yaml` and generate a new template? (yes/no) ")
+        if i.strip() == "yes":
+            os.remove("handmade_dictionary.yaml")
+            processor.handmade_dictionary_template_generator.main(analysis)
+    else:
+        processor.handmade_dictionary_template_generator.main(analysis)
+
+    processor.data_mover.main(analysis)
+
 
 if __name__ == "__main__":
-  main()
+    main()
--- francophonic/sources/handmade-dictionaries/processor/utils.py
+++ francophonic/sources/handmade-dictionaries/processor/utils.py
@@ -9,6 +9,7 @@ except ImportError:
     from yaml import Loader, Dumper
 
 import safer
+import uuid
 
 
 def line_to_words(line, contractions, whole_word_basis):
@@ -20,7 +21,7 @@ def line_to_words(line, contractions, whole_word_basis):
                 if word != "-":
                     if word in contractions.keys():
                         new_words.extend(contractions[word])
-                    else: 
+                    else:
                         new_words.append(word)
             return new_words
         return [w]
@@ -31,32 +32,36 @@ def line_to_words(line, contractions, whole_word_basis):
     words = [word for word in words if word not in ['', '–', ' ']]
     return words
 
+
 def clean_line(line):
     line = line.replace('’', "'")
     line = line.strip()
     #line = line.strip("“–«» ")
     #line = line.strip()
-    line = re.sub('\[.*?]', '', line) # get rid of the timestamps in bits like "[00:00:12] Bonjour à tous ou bonsoir"
+    line = re.sub('\[.*?]', '', line)  # get rid of the timestamps in bits like "[00:00:12] Bonjour à tous ou bonsoir"
     line = " ".join(line.split())
     line = re.sub('^[^ ]*? : ', '', line)
     # line = line.strip("“–«» ")
-    
-    #if ":" in line:
+
+    # if ":" in line:
     #    return ""
     return line.strip()
 
+
 def retain_only_characters(line):
     line = line.strip()
-    line = re.sub(r"[^A-ZÉÀÂÄÈÉÊËÎÏÔŒÙÛÜŸÇ a-zàâäèéêëîïôœùûüÿç'-]", "", line.replace('’',"'")).strip()
+    line = re.sub(r"[^A-ZÉÀÂÄÈÉÊËÎÏÔŒÙÛÜŸÇ a-zàâäèéêëîïôœùûüÿç'-]", "", line.replace('’', "'")).strip()
     return line
 
 
 def line_to_words_french(line):
     return line_to_words(retain_only_characters(line), frenchContractions, whole_word_basis=False)
 
+
 def line_to_words_english(line):
-    return line_to_words(line, 
-        englishContractions, whole_word_basis=True)
+    return line_to_words(line,
+                         englishContractions, whole_word_basis=True)
+
 
 def get_translations():
     with safer.open("translations.yaml", encoding='utf-8') as f:
@@ -70,8 +75,6 @@ def get_word_dictionary():
     return word_dictionary
 
 
-
-
 def get_all_known_french_words():
     word_dictionary = get_word_dictionary()
     words = set([])
@@ -82,7 +85,7 @@ def get_all_known_french_words():
                 for form, persons in forms.items():
                     words.update(set(persons.values()))
     return words
-            
+
 
 def get_sentence_analysis(analysis):
     (_, collected_sentences, _) = analysis
@@ -98,75 +101,252 @@ def get_sentence_analysis(analysis):
     return understandable_sentences, ununderstandable_sentences
 
 
-
-
 def get_understandable_sentences(analysis):
     return get_sentence_analysis(analysis)[0]
 
 
-flatten = lambda l: [item for sublist in l for item in sublist]
+def flatten(l): return [item for sublist in l for item in sublist]
+
+
 def flatmap(l, f):
     return flatten([f(e) for e in l])
+
+
 def flatdmap(d, f):
     flatten([f(k, v) for k, v in d.items])
 
 
+def french_conj_description_to_english(french_word, french_mode, french_form, french_person, conjugations_english, english_infinitive):
+    # print(f"Finding an english conjugation for the word {french_word} ({french_mode} {french_form} {french_person})")
+
+    if french_mode == "infinitif":
+        return english_infinitive
+
+    modes = {
+        'indicatif': ('indicative', {
+            'présent': 'present',
+            'imparfait': 'past continous',
+            'futur': 'future',  # this is a guess
+            'passé simple': 'preterite',
+            'passé composé': 'preterite',
+            'plus-que-parfait': 'past perfect',
+            'passé antérieur': 'past perfect continuous',  # only used in literature, as far as I can tell has no direct analogue in english
+            'futur antérieur': 'future perfect continuous',
+        }),
+        'subjonctif': ('indicative', {  # I don't think english has a subjunctive mood, let's just map these to english as if they were indicative
+            'présent': 'present',
+            'imparfait': 'past continous',
+            'plus-que-parfait': 'past perfect',
+            'passé': 'preterite',
+            # I also don't think english has a conditional mood, you just put "would" before the verb. That's reflected in the person, we'll have to get that later
+        }), 'conditionnel': ('indicative', {
+            'présent': 'present',
+            'passé première forme': 'present perfect',
+            'passé deuxième forme': 'present perfect',  # Only ever used in excessively fancy writing
+        }), 'participe': ('participle', {
+            'présent': 'present',
+            'passé composé': 'past',
+            'passé': 'past',
+        }), 'impératif': ('imperative', {  # These are easy - english has no imperative tenses
+            'présent': '',
+            'passé': '',
+        }), 'infinitif': ('infinitive', {
+            'présent': '',
+            'passé': '',
+        })}
+
+    persons = {
+        'je': 'I',
+        'j\'': 'I',
+        'tu': 'you',
+        'il/elle': 'he/she/it',
+        'elle': 'he/she/it',
+        'il': 'he/she/it',
+        'nous': 'we',
+        'vous': 'you',
+        'ils/elles': 'they',
+        'ils': 'they',
+        'elles': 'they',
+
+
+        'ayant': '',
+        'étant': '',
+
+        'aie': '',
+        'ayons': '',
+        'ayez': '',
+        'sois': '',
+        'soyons': '',
+        'soyez': '',
+
+
+        'masc.sg.:': '',
+        'masc.pl.:': '',
+        'fém.sg.:': '',
+        'fém.pl.:': '',
+
+        '': '',
+    }
+
+    english_mode, tenses = modes[french_mode]
+    english_tense = tenses[french_form]
+    english_person = None
+    for i in french_person:
+        if i in persons:
+            english_person = persons[i]
+            break
+
+    if english_person == None:
+        raise Exception(f"For french word '{french_word}', I couldn't find any of '{french_person}' in the dictionary mapping french persons to english person")
+
+    # for error detection
+    if conjugations_english.get(english_mode, None) == None:
+        raise Exception(f"The english conjugation in the mood {english_mode} for the french word {french_word} was not found in the dictionary {conjugations_english}")
+    if conjugations_english[english_mode].get(english_tense, None) == None:
+        raise Exception(
+            f"The english conjugation in the tense {english_tense} for the french word {french_word} was not found in the dictionary {conjugations_english[english_mode]}")
+    #
+
+    possible_persons = {tuple(k.split(' - ')): v for k, v in conjugations_english[english_mode][english_tense].items()}
+    if len(possible_persons) == 1:
+        return list(possible_persons.values())[0]
+    for possible_person in possible_persons:
+        if english_person.lower() in list(possible_person):
+            return possible_persons[possible_person]
+    else:
+        raise Exception(
+            f"Couldn't find an english conjugation for the word '{french_word}'. {french_mode} ({english_mode}) - {french_form} ({english_tense}) {french_person} ({english_person})). Possible persons were {list(possible_persons.keys())}")
+
+
+def french_description_to_english_modal(french_word, french_mode, french_form, french_person, conjugations_english, english_infinitive):
+    if french_mode == "infinitif":
+        return english_infinitive
+
+    if french_form in ['présent']:
+        return conjugations_english['present']
+
+    if french_form in ['imparfait', 'passé simple', 'passé composé', 'plus-que-parfait', 'passé antérieur', 'passé première forme', 'passé deuxième forme', 'passé']:
+        return conjugations_english['past']
+
+    if french_form in ['futur', 'futur antérieur']:
+        return conjugations_english['future']
+
+    raise Exception(f"{french_word}'s {french_form} form wasn't understood ")
+
+
+def make_useful_dictionary(word_dict):
+    output_words = {'french': {}, 'english': {}}
+
+    seen_uuids = set()
+
+    for french_word, entry in word_dict['french'].items():
+        if entry['uuid'] in seen_uuids:
+            raise Exception(f"UUID for {french_word} already in dictionary!")
+        else:
+            seen_uuids.add(entry['uuid'])
+        for definition in entry.get('definitions', {}):
+            if definition.get('pos', None) == 'verb':
+                definition['isInfinitive'] = True
+            output_words['french'][french_word] = output_words['french'].get(french_word, {'definitions': [], 'occurrences': 0, 'uuid': entry['uuid']})
+            output_words['french'][french_word]['definitions'] += [definition]
+
+            if definition.get('pos', '') == 'verb':
+                reversed_dict = {}
+                for mode, formes in definition['conjugations_french'].items():
+                    for forme, persons in formes.items():
+                        for person, conjugation in persons.items():
+                            reversed_dict[conjugation] = reversed_dict.get(conjugation, {
+                                'uuid': uuid.uuid5(uuid.NAMESPACE_DNS, f"{french_word}.{conjugation}."),
+                                'definition': {
+                                    'display': conjugation,
+                                    'pos': 'verb',
+                                    'infinitive': french_word,
+                                    'isInfinitive': False,
+                                    'conjugates_for': [],
+                                    'translations': set(),
+                                    'gender': 'NA',
+                                }
+                            })
+                            person = person.split(" - ")
+                            reversed_dict[conjugation]['definition']['conjugates_for'].append((mode, forme, person))
+
+                            english_infinitive = definition['translations'][0]
+                            english_conjugations = definition['conjugations_english'][english_infinitive]
+
+                            english_translation = french_conj_description_to_english(french_word, mode, forme, person, english_conjugations, english_infinitive) if not definition['modal_in_english'] else french_description_to_english_modal(
+                                french_word, mode, forme, person, english_conjugations, english_infinitive)
+
+                            reversed_dict[conjugation]['definition']['translations'].add(english_translation)
+                for french_word, entry in reversed_dict.items():
+                    entry['definition']['translations'] = list(entry['definition']['translations'])
+                    entry['definitions'] = [entry['definition']]
+                    del entry['definition']
+                    for translation in definition.get('translations', []):
+                        output_words['english'][translation] = output_words['english'].get(translation, []) + [french_word]
+
+                    output_words['french'][french_word] = output_words['french'].get(french_word, {'definitions': [], 'occurrences': 0, 'uuid': str(entry['uuid'])})
+                    output_words['french'][french_word]['definitions'] += entry['definitions']
+
+            else:
+                for translation in definition.get('translations', []):
+                    output_words['english'][translation] = output_words['english'].get(translation, []) + [french_word]
+        # if french_word[0] != 'a': break
+    return output_words
+
+
 frenchContractions = {
-    'n': ['ne'], 
-    'l': ['le'], 
-    'lorsqu': ['lorsque'], 
-    'j': ['je'], 
-    's': ['se'], 
-    'jusqu': ['jusque'], 
-    'quelqu': ['quelque'], 
-    'm': ['me'], 
-    'c': ['ce'], 
-    'd': ['de'], 
-    'qu': ['que'], 
-    't': ['te'], 
+    'n': ['ne'],
+    'l': ['le'],
+    'lorsqu': ['lorsque'],
+    'j': ['je'],
+    's': ['se'],
+    'jusqu': ['jusque'],
+    'quelqu': ['quelque'],
+    'm': ['me'],
+    'c': ['ce'],
+    'd': ['de'],
+    'qu': ['que'],
+    't': ['te'],
     'm': ['me']
 }
 englishContractions = {
-    "aren't":    ["are"    , "not"  ],
-    "can't":     ["can"    , "not"  ],
-    "couldn't":  ["could"  , "not"  ],
-    "shouldn't": ["should" , "not"  ],
-    "wouldn't":  ["would"  , "not"  ],
-    "didn't":    ["did"    , "not"  ],
-    "doesn't":   ["does"   , "not"  ],
-    "hadn't":    ["had"    , "not"  ],
-    "hasn't":    ["has"    , "not"  ],
-    "haven't":   ["have"   , "not"  ],
-    "mightn't":  ["might"  , "not"  ],
-    "shan't":    ["shall"  , "not"  ],
-    "weren't":   ["were"   , "not"  ],
-    "won't":     ["will"   , "not"  ],
-    "isn't":     ["is"     , "not"  ],
-    "he'll":     ["he"     , "will" ],
-    "she'll":    ["she"    , "will" ],
-    "you'll":    ["you"    , "will" ],
-    "they'll":   ["they"   , "will" ],
-    "what'll":   ["what"   , "will" ],
-    "who'll":    ["who"    , "will" ],
-    "i'd":       ["i"      , "had"  ],
-    "she'd":     ["she"    , "had"  ],
-    "he'd":      ["he"     , "had"  ],
-    "they'd":    ["they"   , "had"  ],
-    "i'm":       ["i"      , 'am'   ],
-    "i've":      ['i'      , 'had'  ],
-    "we've":     ["we"     , "have" ],
-    "you've":    ["you"    , "have" ],
-    "they've":   ["they"   , "have" ],
-    "let's":     ['let'    , 'us'   ],
-    "that's":    ["that"   , "is"   ],
-    "there's":   ["there"  , "is"   ],
-    "what's":    ["what"   , "is"   ],
-    "he's":      ["he"     , "is"   ],
-    "she's":     ["she"    , "is"   ],
-    "they're":   ["they"   , "are"  ],
-    "you're":    ["you"    , "are"  ],
-    "we're":     ["we"     , "are"  ],
+    "aren't":    ["are", "not"],
+    "can't":     ["can", "not"],
+    "couldn't":  ["could", "not"],
+    "shouldn't": ["should", "not"],
+    "wouldn't":  ["would", "not"],
+    "didn't":    ["did", "not"],
+    "doesn't":   ["does", "not"],
+    "hadn't":    ["had", "not"],
+    "hasn't":    ["has", "not"],
+    "haven't":   ["have", "not"],
+    "mightn't":  ["might", "not"],
+    "shan't":    ["shall", "not"],
+    "weren't":   ["were", "not"],
+    "won't":     ["will", "not"],
+    "isn't":     ["is", "not"],
+    "he'll":     ["he", "will"],
+    "she'll":    ["she", "will"],
+    "you'll":    ["you", "will"],
+    "they'll":   ["they", "will"],
+    "what'll":   ["what", "will"],
+    "who'll":    ["who", "will"],
+    "i'd":       ["i", "had"],
+    "she'd":     ["she", "had"],
+    "he'd":      ["he", "had"],
+    "they'd":    ["they", "had"],
+    "i'm":       ["i", 'am'],
+    "i've":      ['i', 'had'],
+    "we've":     ["we", "have"],
+    "you've":    ["you", "have"],
+    "they've":   ["they", "have"],
+    "let's":     ['let', 'us'],
+    "that's":    ["that", "is"],
+    "there's":   ["there", "is"],
+    "what's":    ["what", "is"],
+    "he's":      ["he", "is"],
+    "she's":     ["she", "is"],
+    "they're":   ["they", "are"],
+    "you're":    ["you", "are"],
+    "we're":     ["we", "are"],
 }
-
-
-
