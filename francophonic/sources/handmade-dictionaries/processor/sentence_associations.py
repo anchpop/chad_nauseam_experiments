8d41e1656d29bb27d0c2831c50eed54e00a89e91
Andre Popovitch <andre@popovit.ch>
2020-11-18 13:47:17 -0500

    been banging my head against the wall trying to figure out a good way to do cross-translation subphrase association, is this the thing that finally does me in?

--- francophonic/sources/handmade-dictionaries/processor/sentence_associations.py
+++ francophonic/sources/handmade-dictionaries/processor/sentence_associations.py
@@ -1,18 +1,43 @@
 """
+from collections import namedtuple
+
+import spacy
+from spacy import displacy
 from processor.utils import *
 
 sentences = get_translations()['french_to_english']
 word_dict = get_word_dictionary()
 useful_word_dict = make_useful_dictionary(word_dict)
 
+nlp_en = spacy.load("en_core_web_lg")
+nlp_fr = spacy.load("fr_core_news_lg")
+
+
+Sentence = namedtuple('Sentence', ['fr', 'en'])
+Words = namedtuple('Words', ['fr', 'en'])
 
 def run():
-    wordsf, wordse = (lambda x: (line_to_words_french(x[0]), line_to_words_english(x[1]['google'][0])))(list(sentences.items())[0])
+    firstPair = [(k, v) for k, v in sentences.items() if '"' not in k and '-' not in k][0]
+    sent = Sentence(fr=firstPair[0], en=firstPair[1]['google'][0])
+    words = Words(fr=line_to_words_french(sent.fr), en=line_to_words_english(sent.en))
+    print(f"{len(words.fr)} - {sent.fr} - {' '.join(words.fr)}")
+    print(f"{len(words.en)} - {sent.en} - {' '.join(words.en)}")
+    print()
+
+
+    # doc_en = nlp_en(sent.en)
+    # displacy.serve(doc_en, style="dep")
+
+    # doc_fr = nlp_fr(sent.fr)
+    # displacy.serve(doc_fr, style="dep")
+
+
     used_indices = []
     output = []
-    print(" ".join(wordsf))
-    print(" ".join(wordse))
-    for wordf in wordsf:
+
+    for wordf in words.fr:
+        break
         translations = [translation for definition in useful_word_dict['french'][wordf]['definitions'] for translation in definition.get('translations', [wordf])]
         print(f"{wordf}: {translations}")
+run()
 """
